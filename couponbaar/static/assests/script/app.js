2b437e5b4f79d2c0441ab14a7ab1b5ad800612c0
Ashish <karankmishra1408@gmail.com>
2023-09-07 11:04:34 +0530

    07_Sept_2023_Update

--- couponbaar/static/assests/script/app.js
+++ couponbaar/static/assests/script/app.js
@@ -220,53 +220,45 @@ const slider = document.querySelector("[data-slider]");
 // // }, 1000); // Change slide every 3 seconds
 
 
-const wrapper = document.querySelector(".deal-container");
-    const carousel = document.querySelector(".best-deal-wrapper");
-    const firstCardWidth = carousel.querySelector(".deal-coupon").offsetWidth;
+function initializeCarousel(wrapperSelector, carouselSelector, cardSelector, arrowBtnSelector) {
+    const wrapper = document.querySelector(wrapperSelector);
+    const carousel = document.querySelector(carouselSelector);
+    const firstCardWidth = carousel.querySelector(cardSelector).offsetWidth;
 
-    const arrowBtns = document.querySelectorAll(".deal-container .arrowBtn i");
+    const arrowBtns = document.querySelectorAll(arrowBtnSelector);
     const carouselChildrens = [...carousel.children];
-
 
     let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
-    // Get the number of cards that can fit in the carousel at once
     let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-    // Insert copies of the last few cards to beginning of carousel for infinite scrolling
     carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
         carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
     });
 
-    // Insert copies of the first few cards to end of carousel for infinite scrolling
     carouselChildrens.slice(0, cardPerView).forEach(card => {
         carousel.insertAdjacentHTML("beforeend", card.outerHTML);
     });
 
-    // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
     carousel.classList.add("no-transition");
     carousel.scrollLeft = carousel.offsetWidth;
     carousel.classList.remove("no-transition");
 
-    // Add event listeners for the arrow buttons to scroll the carousel left and right
     arrowBtns.forEach(btn => {
         btn.addEventListener("click", () => {
             carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
         });
     });
 
-
     const dragStart = (e) => {
         isDragging = true;
         carousel.classList.add("dragging");
-        // Records the initial cursor and scroll position of the carousel
         startX = e.pageX;
         startScrollLeft = carousel.scrollLeft;
     }
 
     const dragging = (e) => {
-        if (!isDragging) return; // if isDragging is false return from here
-        // Updates the scroll position of the carousel based on the cursor movement
+        if (!isDragging) return;
         carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
     }
 
@@ -276,27 +268,23 @@ const wrapper = document.querySelector(".deal-container");
     }
 
     const infiniteScroll = () => {
-        // If the carousel is at the beginning, scroll to the end
         if (carousel.scrollLeft === 0) {
             carousel.classList.add("no-transition");
             carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
             carousel.classList.remove("no-transition");
         }
-        // If the carousel is at the end, scroll to the beginning
         else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
             carousel.classList.add("no-transition");
             carousel.scrollLeft = carousel.offsetWidth;
             carousel.classList.remove("no-transition");
         }
 
-        // Clear existing timeout & start autoplay if mouse is not hovering over carousel
         clearTimeout(timeoutId);
         if (!wrapper.matches(":hover")) autoPlay();
     }
 
     const autoPlay = () => {
-        if (window.innerWidth < 400 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-        // Autoplay the carousel after every 2500 ms
+        if (window.innerWidth < 400 || !isAutoPlay) return;
         timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 1500);
     }
     autoPlay();
@@ -307,6 +295,12 @@ const wrapper = document.querySelector(".deal-container");
     carousel.addEventListener("scroll", infiniteScroll);
     wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
     wrapper.addEventListener("mouseleave", autoPlay);
+}
+
+// Usage example:
+initializeCarousel(".deal-container1", ".best-deal-wrapper1", ".deal-coupon1", ".deal-container1 .arrowBtn i");
+initializeCarousel(".deal-container2", ".best-deal-wrapper2", ".deal-coupon2", ".deal-container2 .arrowBtn i");
+
 
 
     function reveal() {
